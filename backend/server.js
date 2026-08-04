ef0b83eccc322d3e196266f0bd9a18141be5cf91
Bzhar Kamal Abdulazeez <bbbzharkd200@gmail.com>
2026-08-04 10:56:16 +0300

    got the imports for the backend and setup db.js and server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./db");
+
+const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+db.connect((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log("Connected to MySQL!");
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
