315a42f9b6bb08fe49226f79a041d1ce40cf4c6c
Bzhar Kamal Abdulazeez <bbbzharkd200@gmail.com>
2026-08-05 18:15:03 +0300

    changed the create use db to promise

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,7 @@ const db = require("../db");
 const e = require("express");
 //check class exist as well
 
-const createStudentController = (req, res) => {
+const createStudentController = async (req, res) => {
     const role = "student"
     const { Username, Email, Password, DoB, Class_id } = req.body;
     if (Username.trim() == "" ||
@@ -16,34 +16,21 @@ const createStudentController = (req, res) => {
         !Class_id) {
         return res.status(400).json({ error: `invalid inputs` })
     }// then check whether email exists already or no
-    db.query('SELECT * FROM users where email=?', [Email.trim().toLowerCase()], async (err, result) => {
-        if (err) {
-            return res.status(400).json({ error: err })
-        } else if (result.length == 1) {
-            return res.status(409).json({ error: `Email already exists` })
+    try {
+        const [emailResult] = await db.query('SELECT * FROM users where email=?', [Email.trim().toLowerCase()]);
+        if (emailResult.length > 0) {
+            return res.status(409).json({ error: `Email already exists` });
         }
-        // check whether the class exist
-        db.query('SELECT * FROM classes where id=?', [Class_id], (err, result) => {
-            if (err) {
-                return res.status(400).json({ error: err })
-            } else if (result.length != 1) {
-                return res.status(409).json({ error: `class does not exist` })
-            }
-        })
-        // then hash the password
-        try {
-            const hashedPassword = await bcrypt.hash(Password, 10);
-            db.query('INSERT INTO users (username,email,password,role,date_of_birth,class_id) VALUES(?,?,?,?,?,?)', [Username.trim(), Email.trim().toLowerCase(), hashedPassword, role, DoB, Class_id], (err, result) => {
-                if (err) {
-                    return res.status(400).json({ error: err })
-                }
-                return res.status(201).json({ message: `Student enrolled successfully` })
-            })
-        } catch (err) {
-            return res.status(400).json({ error: `internal sever error` })
-
+        const [classResult] = await db.query('SELECT * FROM classes where id=?', [Class_id]);
+        if (classResult.length == 0) {
+            return res.status(409).json({ error: `Class doesn't exist` });
         }
-    })
+        const hashedPassword = await bcrypt.hash(Password, 10);
+        await db.query('INSERT INTO users (username,email,password,role,date_of_birth,class_id) VALUES(?,?,?,?,?,?)', [Username.trim(), Email.trim().toLowerCase(), hashedPassword, role, DoB, Class_id]);
+        return res.status(201).json({ message: `Student enrolled successfully` })
+    } catch (err) {
+        return res.status(500).json({ error: `internal sever error` })
+    }
 }
 
 const createStaffController = async (req, res) => {
