c6c162fac1003622637bb59a4c2ee4b639d07e8f
Bzhar Kamal Abdulazeez <bbbzharkd200@gmail.com>
2026-09-08 11:42:48 +0300

    added helmet to configure csp

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 
 const db = require("./db");
 
@@ -15,6 +16,8 @@ app.use(cors({
     origin: "http://localhost:5173"
 }));
 app.use(express.json());
+
+app.use(helmet());
 
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
